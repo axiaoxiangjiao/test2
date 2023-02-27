4bebbfb28c9849c2724b77dfc2fe9c08a6c0294f
1441351804@qq.com <1441351804@qq.com>
2023-02-27 16:56:11 +0800

    pp

--- main.tf
+++ main.tf
@@ -1,23 +1,10 @@
 
+data "alicloud_account" "account_id" {
 
-# data "alicloud_account" "account_id" {
-
-# }
-
-# output "account_id" {
-#   value = data.alicloud_account.account_id.id
-# }
-
-
-
-
-module "TEST" {
-  source  = "app.terraform.io/hbw-test/nocode/alicloud"
-  version = "1.0.0"
 }
-
-
 
 output "account_id" {
-  value = module.TEST.account_id
+  value = data.alicloud_account.account_id.id
 }
+
+
